6c6c9c89fda5f535095fbb255501dccf9ebb8065
fdsea <uncle-4040@yandex.ru>
2017-10-06 12:27:07 +0400

    add bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-
+import { Button } from 'react-bootstrap';
 import 'react-select/dist/react-select.css';
 
 class Exp extends React.Component{
 
 	render(){
 		return(
-			<div>Hello!!!!</div>
+			<div>
+				<Button />
+			</div>
 		);
 	}
 }
 
-render(<Exp />, document.getElementById('app-root'));
+render(<Exp/>, document.getElementById('app-root'));
