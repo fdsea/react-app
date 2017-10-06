eadf4ba41a6dbdafa982d46318ec7c1d0ab1b4cb
fdsea <uncle-4040@yandex.ru>
2017-10-06 15:08:25 +0400

    router

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,9 @@ import { render } from 'react-dom';
 import { Button } from 'react-bootstrap';
 import ButtonA from './button';
 import CustomersTable from './customersTable';
+
+import { BrowserRouter , Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import 'react-select/dist/react-select.css';
 
 
